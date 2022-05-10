e6a33f31183b4449798d352f11e2d400c82946bf
Mohamed Fakhreddine Mouelhi <94762437+Mangetsu0000@users.noreply.github.com>
2022-05-10 15:11:21 +0100

    Configured jwtStrategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, RecipeModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    RecipeModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(confing: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nestn?schema=public',
+          url: confing.get('DATABASE_URL'),
         },
       },
     });
